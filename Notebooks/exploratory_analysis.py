059d13328b2b532e538e4e29cd4dcdf50305355d
rcotesta <rcotest1@jh.edu>
2022-10-05 17:39:24 -0400

    Add updated EDA

--- Notebooks/exploratory_analysis.py
+++ Notebooks/exploratory_analysis.py
@@ -83,9 +83,9 @@ heatmap.set_title('Correlation of features with Sales Price', fontdict={'fontsiz
 # 
 # Some of these feature are likely strongly correlated, such as "GarageCars" and "GarageArea"
 
-# Let's explore more this correlation between features
+# Let's explore more this correlation between numerical features
 
-# In[50]:
+# In[9]:
 
 
 mask = np.triu(correlations_train)
@@ -104,20 +104,61 @@ plt.show()
 
 # This confirms that variables like "GarageArea" and "GarageCars" are strongly correlated. Maybe it makes sense to consider using only one of them and reduce the dimensionality of the problem
 
+# Let's look now at the importance of categorical variables for the estimating price
+
+# In[10]:
+
+
+from matplotlib.ticker import MaxNLocator
+def boxplots_cat(y, df):
+    
+    '''Create boxplots for categorical variables'''
+    
+    ncol = 3
+    nrow = int(np.ceil(len(df.select_dtypes(include=['object']).columns)/ncol))
+    
+    fig, axes = plt.subplots(nrow, ncol, figsize=(5*ncol, 5*nrow))
+    axes = axes.flatten()
+
+    for i, j in zip(df.select_dtypes(include=['object']).columns, axes):
+
+        sortd = df.groupby([i])[y].median().sort_values(ascending=False)
+        sns.boxplot(x=i,
+                    y=y,
+                    data=df,
+                    palette='magma',
+                    order=sortd.index,
+                    ax=j)
+        j.tick_params(labelrotation=45)
+        j.yaxis.set_major_locator(MaxNLocator(nbins=18))
+
+        plt.tight_layout()
+
+
+# In[11]:
+
+
+boxplots_cat('SalePrice', train)
+
+
+# Some categories like "LotConfig" and "LandSlope" don't seem to make a big difference. Maybe it's worth thinking about
+# reducing the dimensionality of the model by removing them
+
 # ## Data preparation
 
 # Split dataset in X and y
 
-# In[20]:
+# In[12]:
 
 
 y = train['SalePrice'].reset_index(drop=True)
 X = train.drop(['SalePrice'], axis=1)
+X = pd.concat([X,test]).reset_index(drop=True)
 
 
 # Let's deal with missing data first
 
-# In[23]:
+# In[13]:
 
 
 def fraction_missing_data(df):
@@ -140,13 +181,13 @@ def fraction_missing_data(df):
     return pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
 
 
-# In[24]:
+# In[14]:
 
 
 missing_data = fraction_missing_data(X)
 
 
-# In[38]:
+# In[15]:
 
 
 fig, ax = plt.subplots(figsize=(20, 5))
@@ -155,7 +196,7 @@ plt.xticks(rotation=90)
 plt.show()
 
 
-# In[42]:
+# In[16]:
 
 
 X[missing_data.index]
@@ -164,10 +205,10 @@ X[missing_data.index]
 # As anticipated, for some categories we seem to have missing data. In reality pandas is just misinterpreting the "NA"s.
 # Let's take care of them first
 
-# In[44]:
+# In[17]:
 
 
-# Categories where 'NaN' mean None.
+# Categories where 'NaN's mean None.
 
 none_cols = ['Alley', 'PoolQC', 'MiscFeature', 'Fence', 'FireplaceQu', 'GarageType',
             'GarageFinish', 'GarageQual', 'GarageCond', 'BsmtQual', 'BsmtCond',
@@ -178,7 +219,7 @@ for col in none_cols:
     X[col].replace(np.nan, 'None', inplace=True)
 
 
-# In[46]:
+# In[18]:
 
 
 missing_data = fraction_missing_data(X)
@@ -188,35 +229,108 @@ plt.xticks(rotation=90)
 plt.show()
 
 
-# In[47]:
+# In[19]:
 
 
 X[missing_data.index]
 
 
-# In[3]:
+# In[20]:
+
+
+# Categories where 'NaN's mean 0.
+
+zero_cols = [
+    'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'BsmtFullBath',
+    'BsmtHalfBath', 'GarageYrBlt', 'GarageArea', 'GarageCars', 'MasVnrArea'
+]    
+
+for col in zero_cols:
+    X[col].replace(np.nan, 0, inplace=True)
+
+
+# In[21]:
+
+
+missing_data = fraction_missing_data(X)
+fig, ax = plt.subplots(figsize=(20, 5))
+sns.barplot(x=missing_data.index, y='Percent', data=missing_data, palette='Oranges_r')
+plt.xticks(rotation=90)
+plt.show()
+
+
+# In[22]:
+
+
+X[missing_data.index]
+
+
+# In[23]:
+
+
+# Categories where 'NaN's are actually missing data. I replace the categorical ones with the mode
+# the numerical one (LotFrontage) I replace with the median
+
+freq_cols = [
+    'Electrical', 'Exterior1st', 'Exterior2nd', 'Functional', 'KitchenQual',
+    'SaleType', 'Utilities','MSZoning'
+]
+
+
+# In[24]:
+
+
+for col in freq_cols:
+    X[col].replace(np.nan, X[col].mode()[0], inplace=True)
+
+X['LotFrontage'].replace(np.nan, X['LotFrontage'].median(), inplace=True)
+
+
+# Let's drop the useless categories I found before
+
+# In[25]:
+
+
+X.drop(['LotConfig','LandSlope','GarageArea'],axis=1,inplace=True)
+
+
+# Let's find the categorical variables to encode them
+
+# In[26]:
 
 
 # Find the categorical columns
 
 categorical_columns_selector = selector(dtype_include=object)
-categorical_columns = categorical_columns_selector(train)
-
-
-# Split variables in dependent and independent
-
-X = train.iloc[:,:-1].values
-y = train.iloc[:,-1].values
+categorical_columns = categorical_columns_selector(X)
 
 # OneHot encoding of the categorical variables
 ## Find indices associated to categorical variables
-indices_of_categorical_columns = np.array([np.where(train.keys() == key)[0][0] for key in categorical_columns])
+indices_of_categorical_columns = np.array([np.where(X.keys() == key)[0][0] for key in categorical_columns])
 ## Apply OneHot encoding
+X = X.values
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(sparse=False), indices_of_categorical_columns)], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 
 
-# In[4]:
+# After taking care of missing data, let's plit again Xs in train and test set for submission
+
+# In[27]:
+
+
+X = X[:len(y), :]
+X_test_submission = X[len(train):, :]
+
+
+# Convert the house price in its log, as we want to optimize this quantity
+
+# In[28]:
+
+
+y = np.log10(y.values)
+
+
+# In[29]:
 
 
 # Split variables in training and test sets
@@ -224,31 +338,21 @@ X = np.array(ct.fit_transform(X))
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 
-# In[5]:
-
-
-# Imputing missing values
-from sklearn.impute import SimpleImputer
-imp = SimpleImputer(missing_values=np.nan, strategy='median')
-X_train = imp.fit_transform(X_train)
-X_test = imp.fit_transform(X_test)
-
-
 # ## Fit using random forest regression
 
-# In[6]:
+# In[30]:
 
 
 from sklearn.ensemble import RandomForestRegressor
 
 
-# In[7]:
+# In[31]:
 
 
 rfr = RandomForestRegressor(n_estimators=10,random_state=0)
 
 
-# In[8]:
+# In[32]:
 
 
 rfr.fit(X_train,y_train)
@@ -256,24 +360,24 @@ rfr.fit(X_train,y_train)
 
 # ## Compare against the test set
 
-# In[9]:
+# In[33]:
 
 
 from sklearn.metrics import r2_score
 
 
-# In[10]:
+# In[34]:
 
 
 y_predict = rfr.predict(X_test)
 r2 = r2_score(y_test,y_predict)
 
 
-# In[11]:
+# In[35]:
 
 
 plt.figure(figsize=(10,10))
-plt.scatter(y_test/10**3,y_predict/10**3)
+plt.scatter(10**(y_test)/10**3,10**(y_predict)/10**3)
 plt.xscale('log')
 plt.yscale('log')
 plt.xlabel('True house prize [k$]')
@@ -283,7 +387,7 @@ plt.xlim(60,800)
 plt.ylim(60,800)
 
 
-# In[12]:
+# In[36]:
 
 
 fractional_difference = np.abs((y_predict-y_test)/y_test)
